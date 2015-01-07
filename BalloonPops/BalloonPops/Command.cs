b46d03211a623739d44fa95e198d2e63f02a3895
MalinaD <malina.demirova@gmail.com>
2015-01-07 02:19:40 +0200

    Comments for the Commands

--- BalloonPops/BalloonPops/Command.cs
+++ BalloonPops/BalloonPops/Command.cs
@@ -9,6 +9,9 @@
     {
         string name;
 
+        /// <summary>
+        /// Property of the field name
+        /// </summary>
         public string Name
         {
             get
@@ -21,6 +24,13 @@
             }
         }
 
+        /// <summary>
+        /// This method verify the command from the game with the selected command from the player.
+        /// </summary>
+        /// <param name="input">The initial command selected from the player</param>
+        /// <param name="result">The commands that could be selected from the player</param>
+        /// <returns>Returns true if correct command is selected.
+        /// Returns false ifwrong command is selected.</returns>
         public static bool TryParse(string input, ref Command result)
         {
             if (input == "top")
